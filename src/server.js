d4a70efafa24459e927a6b5c25205f038506f1a3
Willian Ferreira <williangf@hotmail.com>
2019-08-16 00:05:56 -0300

    feat(init): created all routes, controllers and model

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+
+const server = express();
+
+mongoose.connect('mongodb+srv://admin:asdqwe@cluster0-tpog4.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3000);
